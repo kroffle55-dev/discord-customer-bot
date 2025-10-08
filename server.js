d1ea6a5fed3433b65f84e624a9fee957bec2c450
kroffle55-dev <kroffle55@gmail.com>
2025-10-09 00:13:41 +0900

    server.js 업데이트

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get("/", (req, res) => {
   res.send("I'm alive!");
 });
 
-app.listen(3000, () => {
-  console.log("Web server running on port 3000");
+// Render가 제공하는 포트 사용
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
 });
